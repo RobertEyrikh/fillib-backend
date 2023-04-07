5e9d6adaaa7b1cad8699a91530b3bb2ad8697a81
Robert Eyrikh <RobertEyrikh@gmail.com>
2023-04-07 08:53:01 +0300

    production ready

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(router);
+app.use(cors({
+  credentials: true,
+  origin: "http://fillib.eyrikhproductions.ru/"
+}));
+app.use("/api", router);
 
 const start = async () => {
   try {
